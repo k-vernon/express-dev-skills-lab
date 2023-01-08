4ff3908346db54dc73feb91942bcf8065e4c2ceb
k-vernon <kvernon908@gmail.com>
2023-01-08 16:18:53 -0500

    add delete button

--- controllers/skills.js
+++ controllers/skills.js
@@ -19,8 +19,48 @@ function newSkill(req, res) {
   res.render('skills/new')
 }
 
+function create(req, res){
+  console.log(req.body)
+  req.body.learned = true
+  Skill.create(req.body)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error =>{
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function show(req, res){
+  Skill.findById(req.params.id)
+  .then(skill => {
+    res.render('skills/show', {
+      skill: skill
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function deleteTodo(req, res) {
+  Skill.findByIdAndDelete(req.params.id)
+  .then(todo => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
 
 export {
   index,
   newSkill as new,
+  create,
+  show,
+  deleteTodo as delete,
 }
--- models/skills.js
+++ models/skills.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const skillsSchema = new Schema({
-  language: String,
+  text: String,
   learned: Boolean,
 })
 
